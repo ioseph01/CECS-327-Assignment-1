15ecd8fb194093f36d2f9de001963e94231d632f
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2026-02-10 11:52:39 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,11 @@
 
+
+# Command-line interface where a user can:
+# 
+    # search by city and max_price,
+# 
+    # list all available listings,
+# 
+    # exit.
+# 
+# Connects only to the Application Server, never directly to the Data Server.
