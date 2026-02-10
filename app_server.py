b5892572f141948976dc834b8d6d1fe177ce4495
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2026-02-10 11:51:33 -0800

    Update app_server.py

--- app_server.py
+++ app_server.py
@@ -1,1 +1,7 @@
+# Listens on another TCP port for client requests.
 
+# Parses user queries, forwards data requests to the Data Server, ranks/filter results, and implements a cache of recent queries.
+
+# Returns nicely formatted results to clients.
+
+# Using an interceptor, write all the requests and replies in a log file called app_server.log
