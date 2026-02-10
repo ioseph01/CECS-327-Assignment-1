556e1de137a16c4620354104997fc0b9fbca605a
ioseph01 <115436744+ioseph01@users.noreply.github.com>
2026-02-10 11:51:01 -0800

    Update data_server.py

--- data_server.py
+++ data_server.py
@@ -1,1 +1,5 @@
-
+# Listens on a TCP port.
+#
+# Manages a small housing “database” stored in a local JSON file.
+#
+# Supports simple commands from the Application Server (e.g., “LIST”, “SEARCH city=LongBeach max_price=2500”).
